68c9875e8239d0fbf47a2cbad63f995bfc6a1c68
Sanaullah-sarazai <74836459+Sanaullah-sarazai@users.noreply.github.com>
2021-04-03 04:17:39 +0500

    Commit TodoApp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+
+
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
